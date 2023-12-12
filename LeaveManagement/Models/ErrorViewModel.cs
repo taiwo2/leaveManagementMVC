cb21d994dc9d8ee175bb25b863347b6c730429f4
taiwo2 <adetonat@ymail.com>
2023-12-12 23:37:50 +0000

    LeaveMagagement

---
+++ LeaveManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Leavemanagement.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
